71cdc4e17110e84a1cc68eb3ae4e35ff08365cd1
Matthew Valentine <Valentine@Matthews-MacBook-Pro.local>
2017-03-12 00:38:52 -0800

    Change order of static file dirs

--- softwax/settings.py
+++ softwax/settings.py
@@ -147,8 +147,9 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(BASE_DIR, 'orders/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
+
 )
 
 # Simplified static file serving.
