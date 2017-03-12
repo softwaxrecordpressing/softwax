7832c0cce46756aced168a23e61a4bd8c979ef3e
Matthew Valentine <Valentine@Matthews-MacBook-Pro.local>
2017-03-12 00:11:24 -0800

    changes case of temporary.html

--- orders/views.py
+++ orders/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here
 def coming_soon(request):
-    return render(request, 'orders/temporary.html')
+    return render(request, 'orders/Temporary.html')
