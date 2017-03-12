cf7ee26356a3b8961c7fe43b56f979b7d2025554
Matthew Valentine <Valentine@Matthews-MacBook-Pro.local>
2017-03-12 00:13:22 -0800

    add orders static files to settings.py

--- softwax/settings.py
+++ softwax/settings.py
@@ -148,4 +148,5 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'orders/static'),
 )
