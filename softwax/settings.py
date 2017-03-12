78a9aa2e9f1ea088bdeaf2d7159d47d80fb67e15
Matthew Valentine <Valentine@Matthews-MacBook-Pro.local>
2017-03-12 00:35:33 -0800

    Change PROJECT_ROOT to BASE_DIR

--- softwax/settings.py
+++ softwax/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'orders/static'),
+    os.path.join(BASE_DIR, 'orders/static'),
 )
 
 # Simplified static file serving.
